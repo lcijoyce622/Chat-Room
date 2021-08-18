b4fe36ee18f349c5e2c1c677361f172e0235edb1
lcijoyce622 <lcijoyce622@gmail.com>
2021-08-18 22:57:14 +0800

    change style

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
 html {
   body {
     background: linear-gradient(to right, #91eac5, #86a8e7, #7f7fd5);
-    background: -webkit-linear-gradient(to right, #91eac5, #86a8e7, #7f7fd5);
-
     #app {
       font-family: Avenir, Helvetica, Arial, sans-serif;
       -webkit-font-smoothing: antialiased;
